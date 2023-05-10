663edde959e9bfaffc71841a25649c0831044947
root <Diego29012>
2023-05-10 05:30:10 -0700

    Program Code Base

--- opcodes.c
+++ opcodes.c
@@ -34,3 +34,17 @@ void swapOpCode(stack_t **stack, unsigned int line_number)
 	(void) line_number;
 	printf("swap");
 }
+
+void addOpCode (stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
+	printf("add");
+}
+
+void nopOpCode (stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
+	printf("nop");
+}
