c101a15be3ef88c4e3d90afd0fd24efefc6b7fa8
RoPallas <124267669+RoPallas@users.noreply.github.com>
2023-05-12 09:25:43 -0300

    Update instructions_utils.c

--- instructions_utils.c
+++ instructions_utils.c
@@ -2,9 +2,7 @@
 
 unsigned int ex_instruction(char *opCode, unsigned int line, int val, stack_t **stack)
 {
-
 	unsigned int i = 0, n = (unsigned int) val;
-	unsigned int i = 0;
 
 	const instruction_t instructions[] = {
 		{"pall", pallOpCode},
@@ -24,7 +22,6 @@ unsigned int ex_instruction(char *opCode, unsigned int line, int val, stack_t **
 
 	while (instructions[i].opcode != NULL)
 	{
-
 		if (strcmp(opCode, instructions[i].opcode) == 0)
 		{
 			instructions[i].f(stack, line);
