afaa59dab95aa63bd9e51a54bc2fc37efe652fa9
RoPallas <124267669+RoPallas@users.noreply.github.com>
2023-05-12 10:19:18 -0300

    Update main.c

--- main.c
+++ main.c
@@ -41,16 +41,10 @@ void checkInstructions(char *fileName, stack_t **stack)
 		}
 		val = 0;
 		if (only_spaces(file_info.line) == 1)
-		{
-			file_info.n_line++;
-			continue;
-		}
+			file_info.n_line++, continue;
 		file_info.arr = tokenize(file_info.line);
 		if (file_info.arr == NULL)
-		{
-			file_info.n_line++;
-			continue;
-		}
+			file_info.n_line++, continue;
 		if (file_info.arr[1] && is_num(file_info.arr[1]))
 			val = atoi(file_info.arr[1]);
 		success = ex_instruction(file_info.arr[0], file_info.n_line, val, stack);
