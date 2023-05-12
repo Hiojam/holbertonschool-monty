4823bbb806f8af771b1c3332e85b4a8733dca904
Diego Piñeyro <124789249+Diego29012@users.noreply.github.com>
2023-05-12 12:48:24 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,9 @@
 #include "monty.h"
 
 file_t file_info;
-
 int main(int argc, char const *argv[])
 {
 	file_info.stack = NULL;
-	file_info.line = NULL;
-
 	if (argc != 2)
 	{
 		throwCustomError("USAGE: monty file\n");
@@ -15,7 +12,7 @@ int main(int argc, char const *argv[])
 	file_info.file = strdup(argv[1]);
 	if (!file_info.file)
 	{
-		throwCustomError("Error: malloc failed\n");
+		throwCustomError("Error: malloc failed");
 		free_all();
 		exit(EXIT_FAILURE);
 	}
@@ -23,39 +20,27 @@ int main(int argc, char const *argv[])
 	checkInstructions(file_info.file, &file_info.stack);
 	return (0);
 }
-
 void checkInstructions(char *fileName, stack_t **stack)
 {
-	size_t len = 1024;
+	size_t len = 0;
+	unsigned int success = 1;
+	int val, nReads;
 	int val, nReads, success = 1;
-
-	file_info.line = malloc(sizeof(char) * len);
-
-	if (!file_info.line)
-	{
-		throwCustomError("Error: malloc failed\n");
-		free_all();
-		exit(EXIT_FAILURE);
-	}
 
 	file_info.f = fopen(fileName, "r");
 	file_info.n_line = 1;
-	while ((nReads = getline(&file_info.line, &len, file_info.f)) != -1)
-	{
-		if (nReads == -1)
-		{
-			throwCustomError("Error: malloc failed\n");
-			free_all();
-			exit(EXIT_FAILURE);
+void checkInstructions(char *fileName, stack_t **stack)
 		}
 		val = 0;
 		if (only_spaces(file_info.line) == 1)
+			file_info.n_line++, continue;
 		{
 			file_info.n_line++;
 			continue;
 		}
 		file_info.arr = tokenize(file_info.line);
 		if (file_info.arr == NULL)
+			file_info.n_line++, continue;
 		{
 			file_info.n_line++;
 			continue;
