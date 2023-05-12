774cc20076094539502919cd2f56c5f3f052664d
root <Diego29012>
2023-05-12 05:42:01 -0700

    fixed

--- main.c
+++ main.c
@@ -6,13 +6,10 @@ int main(int argc, char const *argv[])
 {
 
 	stack_t *stack = NULL;
-<<<<<<< HEAD
 
 	file_info.stack = NULL;
-=======
-	
-    file_info.stack = NULL;
->>>>>>> 67781d54133f62f9741975e2311235c8f5eb4d5d
+
+	file_info.stack = NULL;
 
 
 	if (argc != 2)
